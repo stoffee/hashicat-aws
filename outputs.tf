cc591a3c97f05b798c8c40a4979d4c280ac73291
stoffee <chris@roadkill.org>
2021-11-05 10:29:13 -0700

    added route53

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,10 @@
 # Outputs file
+#output "catapp_url" {
+#  value = "http://${aws_eip.hashicat.public_dns}"
+#}
+
 output "catapp_url" {
-  value = "http://${aws_eip.hashicat.public_dns}"
+  value = "http://${aws_route53_record.hashicat.fqdn}"
 }
 
 output "catapp_ip" {
